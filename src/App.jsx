8f5b57d02cbb4476c0bf41fbe45ce71407803436
David-111409 <muhammaddawoud610@gmail.com>
2026-03-03 22:44:33 +0200

    final submit for services component with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import { Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
+import Services from './components/services/Services';
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Services />
       {/* <Routes>
          <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
